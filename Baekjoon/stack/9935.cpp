bde2f62480b0287210f98494172855f354cdbb20
minkchoii <grace020820@naver.com>
2025-09-15 17:36:29 +0900

    9935/문자열 폭발/gold4

--- Baekjoon/stack/9935.cpp
+++ Baekjoon/stack/9935.cpp
@@ -11,39 +11,39 @@ int main(){
     string str, bomb, result;
     stack<char> s;
 
-    cin >> str >> bomb;
+    cin >> str >> bomb; //전체문자열, 폭발문자열 입력받아
 
-    int bomb_size=bomb.length();
+    int bomb_size=bomb.length(); //폭발문자열길이구해
     
     for(char c : str ) {
 
-        s.push(c);
+        s.push(c); //일단 스택에 넣어
         
-        if(s.top()==bomb.back() && s.size() >=bomb_size) {
+        if(s.top()==bomb.back() && s.size() >=bomb_size) {  //폭발문자열 끝부분이 방금넣은 값과 같고 전체스택사이즈가 폭발문자열보다 길 때 검사해
         string tmp= ""; //임시저장
 
         for(int i=0; i<bomb_size; i++){
             tmp+=s.top();
-            s.pop();
+            s.pop();  //임시문자열로 일단 꺼내
         }
 
-        reverse(tmp.begin(),tmp.end());
+        reverse(tmp.begin(),tmp.end()); //반대로 꺼내지니까 다시정렬해
 
         if(tmp!=bomb){
             for(char ch : tmp) {
-                s.push(ch);
+                s.push(ch);  //폭발문자열이랑 다른거 확인 시 다시 넣어줌
             }
         }
     }
 }
 
-    result="";
+    result=""; 
     while(!s.empty()){
         result+=s.top();
-        s.pop();
+        s.pop();  //결과문자열꺼내줌
     }
 
-    reverse(result.begin(), result.end());
+    reverse(result.begin(), result.end()); //정렬
 
     if(result.empty()) {
         cout<<"FRULA"<<"\n";
